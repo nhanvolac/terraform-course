4b5b38c84bbe38b0303235559403359fa1f049b5
Vu Le <hoaivu100413@gmail.com>
2022-10-26 00:09:54 +0700

    Update jenkins-init.sh

--- docker-demo-3/scripts/jenkins-init.sh
+++ docker-demo-3/scripts/jenkins-init.sh
@@ -63,7 +63,7 @@ pip install awscli
 echo "Install PIP OK" >> /tmp/jenkins.log
 
 # install terraform
-TERRAFORM_VERSION="0.12.18"
+TERRAFORM_VERSION="1.3.3"
 wget -q https://releases.hashicorp.com/terraform/$${TERRAFORM_VERSION}/terraform_$${TERRAFORM_VERSION}_linux_amd64.zip \
 && unzip -o terraform_$${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
 && rm terraform_$${TERRAFORM_VERSION}_linux_amd64.zip
@@ -72,6 +72,6 @@ echo "Install Terraform OK" >> /tmp/jenkins.log
 
 # clean up
 apt-get clean
-rm terraform_0.12.18_linux_amd64.zip
+rm terraform_1.3.3_linux_amd64.zip
 
 echo "Cleanup OK" >> /tmp/jenkins.log
