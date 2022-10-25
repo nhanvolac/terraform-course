cd5758b3ce03740836f160a647ecaf9f330c1f61
Vu Le <hoaivu100413@gmail.com>
2022-10-25 14:16:02 +0700

    Update ecs.tf

--- docker-demo-3/ecs.tf
+++ docker-demo-3/ecs.tf
@@ -18,7 +18,7 @@ resource "aws_launch_configuration" "ecs-example-launchconfig" {
 
 resource "aws_autoscaling_group" "ecs-example-autoscaling" {
   name                 = "ecs-example-autoscaling"
-  vpc_zone_identifier  = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
+  vpc_zone_identifier  = [aws_subnet.STG-public-1.id, aws_subnet.STG-public-2.id]
   launch_configuration = aws_launch_configuration.ecs-example-launchconfig.name
   min_size             = 1
   max_size             = 1
