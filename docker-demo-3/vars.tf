db564dcf48622cc040472aafdbcc557929a146d2
Vu Le <hoaivu100413@gmail.com>
2022-10-25 14:18:41 +0700

    Update vars.tf

--- docker-demo-3/vars.tf
+++ docker-demo-3/vars.tf
@@ -2,6 +2,12 @@ variable "AWS_REGION" {
   default = "eu-west-1"
 }
 
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
 variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
 }
@@ -20,6 +26,7 @@ variable "ECS_AMIS" {
     us-east-1 = "ami-1924770e"
     us-west-2 = "ami-56ed4936"
     eu-west-1 = "ami-c8337dbb"
+    ap-southeast-1 = "ami-0996cfb3acf118b24"
   }
 }
 
@@ -31,6 +38,8 @@ variable "AMIS" {
     us-east-1 = "ami-01b996646377b6619"
     us-west-2 = "ami-0637e7dc7fcc9a2d9"
     eu-west-1 = "ami-081ff4b9aa4e81a08"
+    ap-southeast-1 = "ami-0d3b2e26119a851ac"
+    # ap-southeast-1 = "ami-0996cfb3acf118b24"
   }
 }
 
@@ -41,4 +50,3 @@ variable "INSTANCE_DEVICE_NAME" {
 variable "JENKINS_VERSION" {
   default = "2.319.2"
 }
-
