442d2b6d81e2061009da5fe4bfd50351e6bac205
Vu Le <hoaivu100413@gmail.com>
2022-10-25 14:14:24 +0700

    Update jenkins-init.sh

--- docker-demo-3/scripts/jenkins-init.sh
+++ docker-demo-3/scripts/jenkins-init.sh
@@ -24,24 +24,33 @@ mkdir -p /var/lib/jenkins
 echo '/dev/data/volume1 /var/lib/jenkins ext4 defaults 0 0' >> /etc/fstab
 mount /var/lib/jenkins
 
+echo "Mount OK" > /tmp/jenkins.log
+
 # install default-jre (needed for ubuntu 18.04)
 apt-get update
-apt-get install -y default-jre
+apt install -y openjdk-17-jre-headless
 
 # install jenkins and docker
+
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
- https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
- /etc/apt/sources.list.d/jenkins.list > /dev/null
+https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+ 
 apt-get update
-apt-get install -y jenkins=${JENKINS_VERSION} unzip docker.io
+apt-get install -y jenkins unzip docker.io
+
+echo "Install Jenkins OK" >> /tmp/jenkins.log
 
 # enable docker and add perms
 usermod -G docker jenkins
 systemctl enable docker
 service docker start
 service jenkins restart
+
+echo "Start Docker and Jenkins OK" >> /tmp/jenkins.log
 
 # install pip
 wget -q https://bootstrap.pypa.io/get-pip.py
@@ -51,12 +60,18 @@ rm -f get-pip.py
 # install awscli
 pip install awscli
 
+echo "Install PIP OK" >> /tmp/jenkins.log
+
 # install terraform
 TERRAFORM_VERSION="0.12.18"
 wget -q https://releases.hashicorp.com/terraform/$${TERRAFORM_VERSION}/terraform_$${TERRAFORM_VERSION}_linux_amd64.zip \
 && unzip -o terraform_$${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
 && rm terraform_$${TERRAFORM_VERSION}_linux_amd64.zip
 
+echo "Install Terraform OK" >> /tmp/jenkins.log
+
 # clean up
 apt-get clean
 rm terraform_0.12.18_linux_amd64.zip
+
+echo "Cleanup OK" >> /tmp/jenkins.log
